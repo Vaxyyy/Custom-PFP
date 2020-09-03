5b47670f64b1596b17cc4d137b24569166a81dae
Vaxyyy <60188922+Vaxyyy@users.noreply.github.com>
2020-09-03 17:59:49 +1000

    :tada: new avatar-package
    whats new
     - testing out packages
     - new structure for `accounts`

---
+++ test/default-package-discord-vaxyyy.js
@@ -0,0 +1,44 @@
+commander.fleet.avatars = [{
+    "name": "Vaxyyy",
+    "enabled": true,
+    "package": "default-discord",
+    "accounts": [{
+            "name": "Vaxenford",
+            "enabled": true,
+            "link": "https://cdn.discordapp.com/avatars/631395619000549408/f8254af039ebaf9c340b8760b3770e84.png?size=128",
+        },
+        {
+            "name": "Vaxy",
+            "enabled": true,
+            "link": "https://cdn.discordapp.com/avatars/631395619000549408/f8254af039ebaf9c340b8760b3770e84.png?size=128",
+        }
+    ]
+},
+{
+    "name": "Gman",
+    "enabled": true,
+    "package": "default-discord",
+    "accounts": [{
+            "name": "Gameinglo",
+            "enabled": false,
+            "link": "https://cdn.discordapp.com/attachments/693009932932349993/745835671230611528/niko_smile.png?size=128",
+        },
+        {
+            "name": "Dreamlight",
+            "enabled": true,
+            "link": "https://cdn.discordapp.com/attachments/693009932932349993/745835671230611528/niko_smile.png?size=128",
+        }
+    ]
+},
+{
+    "name": "<3",
+    "enabled": false,
+    "package": "default-discord",
+    "accounts": [{
+            "name": "LSC",
+            "enabled": true,
+            "link": "https://media.discordapp.net/attachments/747319615566118963/747320697432309862/QQ20200824130458.jpg?size=128",
+        }
+    ]
+}]
+account.rootSave();
